9cd3db1d4e3f1d607d1b1999462fdc796110f6ad
jin <jinlu.hansome@163.com>
2021-03-12 17:38:23 +0800

    移动端代码

--- src/materials/components/case-detail/src/index.vue
+++ src/materials/components/case-detail/src/index.vue
@@ -1,29 +1,12 @@
 <template>
-  <div :cache="cache" :cid="cid" ctype="case-detail" obj="component" class="casedetail">
+  <div class="casedetail">
     <h1>{{casename}}</h1>
     <div class="casestatusbox">
       <span class="status">{{statusinfo}}</span>
+      <span style="display: inline-block;margin-left: 10px;"></span>
       <span class="unit">{{unitinfo}}</span>
     </div>
-    <img :src="caseimg" class="caseimg" alt="">
     <p>案件描述：{{casedes}}</p>
-    <h2>{{relatedper}}</h2>
-    <ul>
-      <li v-for="(value,index) in caseperson" :key="index">
-        <div class="content">
-          <img :src="value.img" alt="">
-          <span class="number">{{value.number}}</span>
-          <span class="place">所在地：{{value.place}}</span>
-        </div>
-      </li>
-    </ul>
-    <div class="btnbox">
-      <div class="btncontent">
-        <input type="button" value="挂起">
-        <input type="button" value="退回">
-        <input type="button" value="结案">
-      </div>
-    </div>
   </div>
 </template>
 
@@ -38,26 +21,10 @@
     },
     data() {
       return {
-        cid: 'C' + 0,
-        cache: '',
-        casename: 'ADSAKD0012990331',
-        statusinfo: '开展中',
-        unitinfo: '长沙市公安厅',
-        caseimg: require('./images/case.png'),
-        casedes: '案件描述案件描述案件描述案件描述案件描述案件描述案件描述案件描述案件描述案件描述案件描述案件描述案件描述案件描述案件描述案件描述案件描述案件描述案件描述案件描述',
-        relatedper: '关联人员',
-        caseperson: [
-          {
-            'img': require('./images/head_grxx.svg'),
-            'number': '172386237192',
-            'place': '南京'
-          },
-          {
-            'img': require('./images/head_grxx.svg'),
-            'number': '172386237192',
-            'place': '南京'
-          }
-        ]
+        casename: '标题标题标题标题标题标题',
+        statusinfo: '环球网  张良菊',
+        unitinfo: '2018.04.23 12:00:00',
+        casedes: '案件描述案件描述案件描述案件描述案件描述案件描述案件描述案件描述案件描述案件描述案件描述案件描述案件描述案件描述案件描述案件描述案件描述案件描述案件描述案件描述'
       }
     },
     mounted: function() {
---
+++ src/materials/components/case-detail/src/index2.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="casedetail">
+    <div class="content" style="position: relative; top: 0px; width: 100%;height: 100%;bottom: 0px;">
+      <iframe :src="url" style="height: 100%;width:100%; frameborder=no" frameborder="no" border="0" marginWidth=0
+              marginHeight=0></iframe>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        url: 'https://www.baidu.com/'
+      }
+    },
+    mounted: function() {
+
+    },
+    destroyed() {
+      console.log('删除组件!!!!!!!!!!!!!')
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style type="text/scss" rel="stylesheet/scss" lang="scss" scoped>
+  @import "./main.scss";
+</style>
